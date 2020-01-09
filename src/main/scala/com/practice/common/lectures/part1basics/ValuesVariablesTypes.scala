d84c3dc84969a461f248c85caac651bc51ac4161
Imran Sarwar <imranbutt.java@gmail.com>
2020-01-09 22:49:01 +0100

    Scala Learning
    Functions

--- src/main/scala/com/practice/common/lectures/part1basics/ValuesVariablesTypes.scala
+++ src/main/scala/com/practice/common/lectures/part1basics/ValuesVariablesTypes.scala
@@ -2,6 +2,23 @@ package com.practice.common.lectures.part1basics
 
 object ValuesVariablesTypes extends App {
   //Inferred by Compiler
+  //Immutable
   val x = 2
-  print(x)
+  println(x)
+
+  // String
+  val str:String = "Hello Scala"
+  println(str)
+
+  val aShort: Short = 22
+  val aBoolean: Boolean = true
+  val aChar: Char = '2'
+  val aLong: Long = 12121212121L
+  val aFloat: Float = 1f
+  val aDouble: Double = 132232.23232
+
+  //variables, they are side effects in Scala
+  var aVar: Int = 10
+  aVar = 12
+
 }
