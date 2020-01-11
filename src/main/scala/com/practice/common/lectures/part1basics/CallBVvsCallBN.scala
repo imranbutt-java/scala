159415bd5000cb058bc043bd652c91187e79aaa0
Imran Sarwar <imranbutt.java@gmail.com>
2020-01-11 11:33:49 +0100

    Part 1
    Part 1 Basics Completed

---
+++ src/main/scala/com/practice/common/lectures/part1basics/CallBVvsCallBN.scala
@@ -0,0 +1,30 @@
+package com.practice.common.lectures.part1basics
+
+object CallBVvsCallBN extends App {
+  def callByValue(time: Long): Unit = {
+    println(s"By Value: $time")
+    println(s"By Value: $time")
+  }
+
+  //Used in Lazy evaluation
+  def callByName(time: => Long): Unit = {
+    println(s"By Name: $time")
+    println(s"By Name: $time")
+  }
+
+  //In callByValue System.nanoTime() is calculated first and then function is called
+  //callByValue(16623704863825L)
+  //In callByName System.nanoTime() is passed as it is to callByName
+  //callByName(System.nanoTime())
+  callByValue(System.nanoTime())
+  callByName(System.nanoTime())
+
+  //Another good example where byName is not evaluated first
+  def infinite(x: Int): Int = 1 + infinite(x)
+  def printFirst(x: Int, y: => Int): Unit = println(x)
+
+  //It crashed before calling printFirst function bcz infinite(20) was calculating value at line:27
+  //printFirst(infinite(20), 21)
+  //Here infinite(20) was not called in printFirst() function so no stack overflow
+  printFirst(21, infinite(20))
+}
---
+++ src/main/scala/com/practice/common/lectures/part1basics/StringOps.scala
@@ -0,0 +1,45 @@
+package com.practice.common.lectures.part1basics
+
+/* Created by imransarwar on 2020-01-11 */
+object StringOps extends App {
+
+  val str: String = "Hello, learning Scala"
+
+  //Java Specific
+  println(str.charAt(4))
+  //substring first param inclusive and second exclusive
+  println(str.substring(4, 8))
+  println(str.split(" "))
+  println(str.replace(" ", "-"))
+  println(str.length)
+
+  //Scala Specific
+  val aNumString: String = "2"
+  val aNumber: Int = aNumString.toInt
+
+  println( 2  +: aNumString :+ 'z')
+  println('a' +: aNumString :+ 'z')
+  println('a' + aNumString + 'z')
+  println(str.reverse)
+  println(str.take(2))
+
+  // S interpolation
+  val name = "Ashtalfa"
+  val age = 8
+  val greeting = s"I am $name and $age years old"
+  val birthday = s"I am $name and going to be ${age + 1} this year"
+
+  println(birthday)
+
+  // F interpolation
+  val speed = 1.2f
+  val myth = f"$name can eat bread with the speed of $speed%2.2f/minute"
+
+  println(myth)
+
+  // Raw interpolation
+  val escaped = "This is a \n new Line"
+  println(raw"This is a \n new Line")
+  println(raw"$escaped")
+
+}
